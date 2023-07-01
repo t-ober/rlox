54eaee78ea1a547e66338c851b8760c6371bb5a3
Thomas <thomas.oberliessen@googlemail.com>
2023-07-01 13:30:04 +0200

    Reformat tokens

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+pub mod token;
 pub mod token_type;
 
 fn main() {
--- src/token.rs
+++ src/token.rs
@@ -1,6 +1,8 @@
-pub struct Token {
+use crate::token_type::TokenType;
+
+pub struct Token<T> {
     pub token_type: TokenType,
     pub lexeme: String,
-    pub literal: Option<Literal>,
+    pub literal: Option<T>,
     pub line: usize,
 }
