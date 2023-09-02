a7f5e814e672b65c8cb018714639141acf7086f2
Thomas <thomas.oberliessen@googlemail.com>
2023-09-02 10:09:28 +0200

    Init

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,3 @@
-pub mod token;
-pub mod token_type;
-
 fn main() {
     println!("Hello, world!");
 }
--- src/token.rs
+++
@@ -1,8 +0,0 @@
-use crate::token_type::TokenType;
-
-pub struct Token<T> {
-    pub token_type: TokenType,
-    pub lexeme: String,
-    pub literal: Option<T>,
-    pub line: usize,
-}
